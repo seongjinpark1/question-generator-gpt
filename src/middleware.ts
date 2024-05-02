586dce7c3e38aa0eb393226cb622606d5f85de41
seongjinpark1 <blackdavil015@gmail.com>
2024-05-03 08:43:18 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,14 @@ export async function middleware(request: NextRequest) {
   const { nextUrl, url } = request;
   const isValidTime = () => {
     const today = dayjs();
-    const studyDate = dayjs('2024-05-03T07:50:00'); // 해당 날짜라 시간만 수정!
+    const studyDate = dayjs('2024-05-03T08:45:00'); // 해당 날짜라 시간만 수정!
     const isWithinRange =
       today.isAfter(studyDate) &&
       today.isBefore(studyDate.add(1, 'hour').add(30, 'minute'));
-    // return isWithinRange ;
-    return true;
+    console.log({ today });
+    console.log({ studyDate });
+    console.log({ isWithinRange });
+    return isWithinRange;
   };
 
   if (!isValidTime() && nextUrl.pathname !== '/') {
